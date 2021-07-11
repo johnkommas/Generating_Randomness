2084b8ec275b54de3ee0af7f096e9140bc644a0c
Ioannis E. Kommas <johnkommas@gmail.com>
2021-07-11 19:28:46 +0300

    Ioannis E. Kommas

--- Generating Randomness/task/predictor/predictor.py
+++ Generating Randomness/task/predictor/predictor.py
@@ -1,1 +1,14 @@
-print('Hello, World!')
+
+def get_input():
+    print("Print a random string containing 0 or 1: \n")
+    data = input()
+    for letter in data:
+        if letter in ['0', '1']:
+            output.append(letter)
+    if len(output) < 100:
+        print(f'Current data length is {len(output)}, {100 - len(output)} symbols left')
+        return get_input()
+    print(f"Final data string:\n{''.join(output)}")
+
+output = []
+get_input()
