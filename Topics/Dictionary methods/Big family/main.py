e95789b2b29bf2013f6d07579f46c730c197f911
Ioannis E. Kommas <johnkommas@gmail.com>
2021-07-18 11:29:52 +0300

    Ioannis E. Kommas

--- Topics/Dictionary methods/Big family/main.py
+++ Topics/Dictionary methods/Big family/main.py
@@ -1,5 +1,16 @@
+import json
 # The following lines create dictionaries from the input. Do not modify them, please
+#auto check
 first_family = json.loads(input())
 second_family = json.loads(input())
 
+# #manual check
+# first_family = {"wife": "Janet", "wife's mother": "Katie", "wife's father": "George"}
+# second_family = {"husband": "Leon", "husband's mother": "Eva", "husband's father": "Gaspard", "husband's sister": "Isabelle"}
 # Work with the 'first_family' and 'second_family' and create a new dictionary
+# making a new dictionary
+
+whole_family = {}
+whole_family.update(first_family)
+whole_family.update(second_family)
+print(whole_family)
